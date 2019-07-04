7f08b6866c68172ca1659404d77912921b4f15e3
syn <vicdemilly@gmail.com>
2019-07-04 09:03:20 +0200

    CSS intermédiaire

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    $xml = simplexml_load_file('source.xml');
+    $id = $_GET['id'];
+    $display = $xml->page[$id - 1];
+    if (!$display){
+        $display = $xml->page[0];
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -6,66 +14,54 @@
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <link rel="stylesheet" href="assets/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Philosopher&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="assets/css/mickael.css">
       <link rel="stylesheet" href="assets/css/sonia.css">
       <link rel="stylesheet" href="assets/css/victor.css">
       <link rel="stylesheet" href="assets/css/romain.css">
-      <title>Projet PHP</title>
+      <title><?= $display->title ?></title>
   </head>
   <body>
-      <!-- Conteneur principal -->
-      <div class="container-fluid">
-          <!-- Entête -->
-          <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-around">
-              <?php
-                $xml = simplexml_load_file('source.xml');
-                $id = $_GET['id'];
-                foreach ($xml as $pages) {
-              ?>
-                    <a href="index.php?id=<?= $pages['id'] ?>"><?= $pages->menu ?></a>
-              <?php } ?>
-            </nav>
-            <?php
-              echo '<br>';
-              switch ($id) {
-                  case 1:
-                      echo $xml->page[0]->title;
-                      echo $xml->page[0]->content;
-                      break;
-                  case 2:
-                      echo $xml->page[1]->title;
-                      echo $xml->page[1]->content;
-                      break;
-                  case 3:
-                      echo $xml->page[2]->title;
-                      echo $xml->page[2]->content;
-                      break;
-                  case 4:
-                      echo $xml->page[3]->title;
-                      echo $xml->page[3]->content;
-                      break;
-                  default:
-                      echo $xml->page[0]->title;
-                      echo $xml->page[0]->content;
-                      break;
-              }
-            ?>
-          </header>
-          <!-- Contenu principal - Corps de la page -->
-          <main>
+    <!-- Entête -->
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark d-flex justify-content-between">
+        <div class="">
+          <a id="logo" href="index.php">Maçonnerie Ocordo</a>
+        </div>
+        <div class="">
+          <?php
+            $xml = simplexml_load_file('source.xml');
+            $id = $_GET['id'];
+            foreach ($xml as $pages){
+          ?>
+          <?php if($pages['id']>1){ ?>
+          <a href="index.php?id=<?= $pages['id'] ?>"><?= $pages->menu ?></a>
+          <?php } ?>
+          <?php } ?>
+        </div>
+      </nav>
+    </header>
+    <!-- Conteneur principal -->
+    <div class="container-fluid">
+      <!-- Contenu principal - Corps de la page -->
+      <main>
+        <div class="d-flex flex-column">
+        <?= $display->content ?>
+        </div>
+      </main>
+    </div>
+    <!-- Pied de page -->
+    <footer>
 
-          </main>
-          <!-- Pied de page -->
-          <footer>
-
-          </footer>
-      </div>
-      <!-- scripts JQuery, Popper, Angular et Bootstrap-->
-      <script src="assets/js/jquery-3.4.0.min.js"></script>
-      <script src="assets/js/popper.min.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <!-- mon script principal -->
-      <script src="assets/js/main.js"></script>
+    </footer>
+    <!-- scripts JQuery, Popper, Angular et Bootstrap-->
+    <script src="assets/js/jquery-3.4.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- mon script principal -->
+    <script src="assets/js/main.js"></script>
   </body>
 </html>
