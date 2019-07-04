ec3e1b9f57c6412feb147b662c39864d5c21895a
syn <vicdemilly@gmail.com>
2019-07-04 15:20:20 +0200

    Finalisation CSS, ajout commentaire de l'index, reponsive vidéo map

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php
+<?php // On charge l'élément XML présent dans le dossier.
     $xml = simplexml_load_file('source.xml');
+    // On récupère l'ID des pages
     $id = $_GET['id'];
-    $display = $xml->page[$id - 1];
+    // On affiche le contenu des pages en fonction de l'ID.
+    $display = $xml->page[$id - 1]; // Les pages commencent à 1, et l'index commence à 0.
     if (!$display){
-        $display = $xml->page[0];
+        $display = $xml->page[0]; // Si notre utilisateur joue avec l'url, il est renvoyé vers la page d'accueil.
     }
 ?>
 <!DOCTYPE html>
@@ -18,29 +20,32 @@
       <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Philosopher&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css">
       <link rel="stylesheet" href="assets/css/mickael.css">
       <link rel="stylesheet" href="assets/css/sonia.css">
       <link rel="stylesheet" href="assets/css/victor.css">
-      <link rel="stylesheet" href="assets/css/romain.css">
       <title><?= $display->title ?></title>
   </head>
   <body>
     <!-- Entête -->
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-dark d-flex justify-content-between">
-        <div class="">
-          <a id="logo" href="index.php">Maçonnerie Ocordo</a>
-        </div>
-        <div class="">
-          <?php
-            $xml = simplexml_load_file('source.xml');
-            $id = $_GET['id'];
-            foreach ($xml as $pages){
-          ?>
-          <?php if($pages['id']>1){ ?>
-          <a href="index.php?id=<?= $pages['id'] ?>"><?= $pages->menu ?></a>
-          <?php } ?>
-          <?php } ?>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <a class="mt-1" id="logo" href="1.html">MAÇONNERIE OCORDO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+                <?php // On crée une boucle pour l'affichage du menu
+                    foreach ($xml as $pages) {
+                ?>
+                    <li class="nav-item">
+                        <?php if($pages['id']>1){ // Avec cette condition, nous retirons "accueil" des menus affichés. Ceci afin de le renommer à notre façon et de le sortir du menu burger.' ?>
+                        <a class="nav-link" href="<?= $pages['id'] ?>.html"><?= $pages->menu ?></a>
+                        <?php } ?>
+                    </li>
+                <?php } ?>
+            </ul>
         </div>
       </nav>
     </header>
@@ -49,7 +54,7 @@
       <!-- Contenu principal - Corps de la page -->
       <main>
         <div class="d-flex flex-column">
-        <?= $display->content ?>
+        <?= $display->content // On affiche le contenu, chaque contenu d'un menu étant à l'intérieur d'une balise "content". ?>
         </div>
       </main>
     </div>
